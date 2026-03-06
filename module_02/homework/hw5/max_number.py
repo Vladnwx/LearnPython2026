a7faf3f7fef416c817081464c379ddef9b1996b7
Vlad <anykey196@gmail.com>
2026-03-06 03:49:05 +0000

    задача 5

--- module_02/homework/hw5/max_number.py
+++ module_02/homework/hw5/max_number.py
@@ -18,10 +18,43 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/max_number/...")
-def max_number():
-    ...
+@app.route("/max_number/<path:numbers_path>")
+def max_number(numbers_path: str):
+    # Разделяем строку по слешу
+    raw_numbers = list(filter(None, numbers_path.split('/')))
+    
+    if not raw_numbers:
+        return "Вы не передали ни одного числа.", 400
+
+    try:
+        # Преобразуем строки в числа
+        numbers = [int(n) for n in raw_numbers]
+        
+        maximum = max(numbers)
+        
+        # Используем тег <i> для курсива, как в условии
+        return f"Максимальное переданное число: <i>{maximum}</i>"
+        
+    except ValueError:
+        return "Ошибка: переданы некорректные данные. Пожалуйста, вводите только числа.", 400
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/')
+def index():
+    # Список всех эндпоинтов
+    links = []
+    for rule in app.url_map.iter_rules():
+        if "static" not in rule.endpoint:
+            url = rule.rule
+            links.append(f'<li><a href="{url}">{url}</a></li>')
+    
+    return f"""
+    <h1>Навигация по модулю 02</h1>
+    <ul>
+        {"".join(links)}
+    </ul>
+    """
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
